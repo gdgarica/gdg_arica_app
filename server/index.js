44783e9925cc3a79b42c76c6e7830cfa3a349aa8
Cristian <cristianflores.ee@gmail.com>
2020-07-20 21:55:46 -0400

    fix connection

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const _routes = require('./app/routes/v1');
+const _mongoSrv = require('./app/lib/mongodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/api/v1', _routes);
 
-require('./app/lib/mongodb');
-
-app.listen(9000, () => {
+app.listen(9000, async () => {
   console.log('Server corriendo xD');
+  await _mongoSrv.init();
 });
